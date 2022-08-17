0be18307b499bde119e3f524a7effe0a05954feb
Vinny Alves <vinicius.a.araujo@outlook.com>
2022-08-17 20:46:30 -0300

    v(?)
    Alterações no D037 - Conversor de Bases Númericas.
    
    Atualização nos códigos.

--- Aula do Guanabara/Exercicios/D037 - Conversor de Bases Númericas.py
+++ Aula do Guanabara/Exercicios/D037 - Conversor de Bases Númericas.py
@@ -7,15 +7,23 @@
 #Estudar base númericas.
 
 print('\033[7m--------------CONVERSÃO DE BASES NÚMERICAS--------------\033[m')
-print('Vamos converter um número para Binário, Octal ou hexadecimal. ')
+print('Vamos converter um número para Binário, Octal ou Hexadecimal. ')
 
-num = float(input('Qual número você deseja converter? '))
+num = int(input('Digite um número inteiro: '))
 print('Opções: ')
 print('1 - Binário. ')
 print('2 - Octal. ')
 print('3 - Hexadecimal. ')
-base = int(input('Qual a base para conversão?' ))
+base = int(input('Qual a base para conversão? ' ))
 
+if base == 1:
+    print(f'{num} convertido para BINÁRIO é {bin(num) [2:]}!')
+elif base == 2:
+    print(f'{num} convertido pra OCTAL é igual a {oct(num) [2:]}!')
+elif base == 3:
+    print(f'{num} convertido para HEXADECIMAL é {hex(num) [2:]}!')
+else:
+    print('Opção inválida, tente novamente!!')
 
 
 print('\033[7m--------------FIM--------------\033[m')
