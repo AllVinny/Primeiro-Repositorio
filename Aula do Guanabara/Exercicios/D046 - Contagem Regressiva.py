d3e1602b5d2648c104b6eb7b13dd4098c5e64f69
Vinny Alves <vinicius.a.araujo@outlook.com>
2022-12-12 20:12:17 -0300

    updt d046

--- Aula do Guanabara/Exercicios/D046 - Contagem Regressiva.py
+++ Aula do Guanabara/Exercicios/D046 - Contagem Regressiva.py
@@ -1,4 +1,5 @@
 #Faça um programa que mostre na tela uma contagem regressiva para o estouro de fogos de artificio, indo de 10 até 0, com uma pause de 1 segundo entre eles. Emoji de fogos estourando.
+
 import emoji
 from time import sleep
 
@@ -6,4 +7,4 @@ from time import sleep
 for contagem in range(11,1,-1):
     print(contagem-1)
     sleep(1)
-print(emoji.emojize('FELIZ ANO NOVO!!! :fireworks:	:sparkler:'))
+print(emoji.emojize(':sparkler::fireworks: FELIZ ANO NOVO!!! :fireworks::sparkler:'))
