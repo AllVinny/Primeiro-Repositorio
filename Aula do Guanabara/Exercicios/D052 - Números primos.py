ce1ba71cc73aed94f252b7ec54b8505d29509ca6
Vinny Alves <vinicius.a.araujo@outlook.com>
2022-12-14 10:14:17 -0300

    Update d052

--- Aula do Guanabara/Exercicios/D052 - Números primos.py
+++ Aula do Guanabara/Exercicios/D052 - Números primos.py
@@ -1,1 +1,4 @@
 #Faça um programa que leia um número inteiro e diga se ele é ou não um número primo. (primo = divisivel só por 1 ou ele mesmo).
+
+#criar if para programa divisivel por 2 (n%2==0: não primo)
+#
