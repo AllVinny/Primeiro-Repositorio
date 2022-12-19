6642afbeaccdcce0e45123f43fc3b613d9381fcd
Vinny Alves <vinicius.a.araujo@outlook.com>
2022-12-18 23:21:27 -0300

    update d060
    Ainda em estudo

--- Aula do Guanabara/Exercicios/D060 - Cálculo do Fatorial.py
+++ Aula do Guanabara/Exercicios/D060 - Cálculo do Fatorial.py
@@ -1,3 +1,11 @@
 #Faça um programa que leia um número qualquer e mostre seu fatorial.
 #Ex: 5! = 5*4*3*2*1 = 120
 #Vale a pena tentar com while e for
+cont = 1
+r = 1
+n = int(input('Digite um número para descobrir seu fatorial: '))
+
+while cont <= n:
+    r *= cont
+    cont += 1
+print(r)
