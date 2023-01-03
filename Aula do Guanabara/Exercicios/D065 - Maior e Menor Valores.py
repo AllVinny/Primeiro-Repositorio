a52766b5e7365c8a45f6f26d8f7fb7106607fd1a
Vinny Alves <vinicius.a.araujo@outlook.com>
2023-01-02 23:39:10 -0300

    D65 Incompleto

--- Aula do Guanabara/Exercicios/D065 - Maior e Menor Valores.py
+++ Aula do Guanabara/Exercicios/D065 - Maior e Menor Valores.py
@@ -1,1 +1,30 @@
-#Crie um programa que leia vários números inteiros pelo teclado. No final da execução, mostre a média entre todos os valores e qual foi o maior e o menor valor lido. O programa deve perguntar ao usuário se ele quer ou não continuar a digitar valores.
+#Crie um programa que leia vários números inteiros pelo teclado. No final da execução,
+# mostre a média entre todos os valores e qual foi o maior e o menor valor lido.
+# O programa deve perguntar ao usuário se ele quer ou não continuar a digitar valores.
+
+n = 0
+cont = 0
+continuar = True
+conti = "S"
+soma = 0
+maior = 0
+menor = 0
+
+while continuar == True:
+    n = int(input('Qual o seu número? '))
+    if n > maior:
+        maior = n
+    elif n < menor:
+        menor = n
+    soma += n
+    cont += 1
+    conti = str(input('Deseja continuar? S/N '))
+    if conti == 'S' or 's':
+        continuar = True
+    if conti == 'N' or 'n':
+        continuar = False
+media = soma/cont
+print(f'Você digitou {cont} valores; ')
+print(f'O maior valor foi {maior}; ')
+print(f'O menor valor foi {menor};')
+print(f'A média dos números inseridos é {media}')
